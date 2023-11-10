8e12d792ca0a12da67963d59c5e82a6fbd106706
degen <jpeckover@gmail.com>
2023-11-09 18:10:24 -0800

    update coins

--- src/components/CardNav/index.tsx
+++ src/components/CardNav/index.tsx
@@ -22,7 +22,7 @@ function Nav({ activeIndex = 0 }: { activeIndex?: number }) {
         <ButtonMenuItem
           id="pool-nav-link"
           as="a"
-          href="https://sideshift.ai/a/ZSK5LlgfW"
+          href="https://sideshift.ai/a/sQ44MnO4h"
           target="_blank"
           rel="noreferrer noopener"
         >
